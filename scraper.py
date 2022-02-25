ec24761b66f238a752a38886ac226b8255a562c9
Daniel van der Schuur <danielvdschuur@gmail.com>
2022-02-25 14:50:37 +0100

    selection // delete

--- scraper.py
+++ scraper.py
@@ -7,11 +7,14 @@ import time
 driver = webdriver.Chrome(ChromeDriverManager().install())
 data = []
 URL = f"https://tracker.gg/valorant/profile/riot/sjonkie%23bluk/matches"
-# LOAD_MORE_BUTTON_XPATH = '//*[@id="browse-itemsprimary"]/.trn-button' 
 
 
 driver.get(URL)
 driver.implicitly_wait(15) #allow some time to fully load, you may tweak accordingly
+
+def get_text(text):
+    text.text
+    return text
 
 def check_exists_by_xpath(xpath):
     time.sleep(2)
@@ -37,8 +40,28 @@ if check_exists_by_xpath('//span[@class="trn-gamereport-list__group-more"]') == 
     matches = driver.find_elements_by_css_selector(r'[class="match__row"]')
     for match in matches:
         print('data append')
-        # print(match.text)
-        data.append(match.text)
+        print(match.text)
+        match__name = match.find_elements_by_css_selector(r'[class="match__name"]')
+        match__time = match.find_elements_by_css_selector(r'[class="match__time"]')
+        match__subtitle = match.find_elements_by_css_selector(r'[class="match__subtile"]')
+        score__won = match.find_elements_by_css_selector(r'[class="score__won"]')
+        score_lost = match.find_elements_by_css_selector(r'[class="score_lost"]')
+        badge = match.find_elements_by_css_selector(r'[class="badge"]')
+        match__name = match__name.text
+        match__time = match__time.text
+        match__subtitle =  match__subtitle.text
+        score__won = score__won.text
+        score_lost = score_lost.text
+        badge = badge.text
+        match = {
+            'map': match__name,
+            'time': match__time, 
+            'mode': match__subtitle,
+            'score_won': score__won,
+            'score_lost': score_lost, 
+            'placement': badge,
+        }
+        data.append(match)
 time.sleep(5)
 
 
@@ -46,3 +69,17 @@ filename = 'data/game_data.json'
 with open (filename, 'w') as outfile:
     json.dump(data, outfile)
 driver.quit()
+
+# {
+#     'map': match__name,
+#     'time': match__time, 
+#     'mode': match__subtitle,
+#     'score_won': score__won,
+#     'score_lost': score_lost, 
+#     'placement': badge,
+#     "kda": ,
+#     'k/d': , 
+#     'hs_perc': ,
+#     'ADR': ,
+#     'ACS': , 
+# }
